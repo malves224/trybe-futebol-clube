aa113c5d4c992da61438b1328a99e8fc1c289a82
Matheus Alves <malves224@gmail.com>
2022-03-16 13:40:53 -0300

    create model club

---
+++ app/backend/src/database/models/club.ts
@@ -0,0 +1,38 @@
+import { DataTypes, Model } from 'sequelize';
+import db from '.';
+// import OtherModel from './OtherModel';
+
+class Club extends Model {
+  id: number;
+
+  clubName: string;
+}
+
+Club.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  clubName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  // ... Outras configs
+  underscored: true,
+  sequelize: db,
+  timestamps: false,
+});
+
+/**
+  * `Workaround` para aplicar as associations em TS:
+  * Associations 1:N devem ficar em uma das instâncias de modelo
+  * */
+
+// OtherModel.belongsTo(Example, { foreignKey: 'campoA', as: 'campoEstrangeiroA' });
+// OtherModel.belongsTo(Example, { foreignKey: 'campoB', as: 'campoEstrangeiroB' });
+
+// Example.hasMany(OtherModel, { foreignKey: 'campoC', as: 'campoEstrangeiroC' });
+// Example.hasMany(OtherModel, { foreignKey: 'campoD', as: 'campoEstrangeiroD' });
+
+export default Club;
