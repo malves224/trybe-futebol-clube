32b4a964549fc4b54a113bfe52097266e38cb685
Matheus Alves <malves224@gmail.com>
2022-03-16 18:54:34 -0300

    cria rota teste

--- app/backend/src/server.ts
+++ app/backend/src/server.ts
@@ -1,6 +1,15 @@
+import { Response, Request } from 'express';
 import { App } from './app';
+// import Club from './database/models/club';
+// import sequelize = require('./database/models/index');
 import 'dotenv/config';
+
+const server = new App();
 
 const PORT = process.env.PORT || 3001;
 
-new App().start(PORT);
+server.app.get('/Login', async (req: Request, res: Response) => {
+  res.status(200).json('ok');
+});
+
+server.start(PORT);
