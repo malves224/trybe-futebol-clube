105b2e8dd6fd1694fb233f3fb7431c6794e51e7f
Matheus Alves <malves224@gmail.com>
2022-03-19 17:11:56 -0300

    corrige problema de depedencia

--- app/backend/src/auth/MiddlewareAuth.ts
+++ app/backend/src/auth/MiddlewareAuth.ts
@@ -1,20 +1,21 @@
 import { NextFunction, Request, Response } from 'express';
 import Jwt from './Jwt';
 
-export default class MiddlewareAuth {
-  jwt = new Jwt();
+const jwt = new Jwt();
 
-  async authToken(req: Request, res: Response, next: NextFunction) {
-    const { Authorization } = req.headers;
-    if (!Authorization) {
+export default class MiddlewareAuth {
+  static async authToken(req: Request, res: Response, next: NextFunction) {
+    const { authorization } = req.headers;
+    if (!authorization) {
       return res.status(401).json({ message: 'Token invalido' });
     }
+
     try {
-      await this.jwt.validate(Authorization as string);
+      await jwt.validate(authorization);
       next();
     } catch (error) {
       if (error instanceof Error) {
-        return res.status(401).json({ message: 'Token invalido' });
+        return res.status(401).json({ message: error.message });
       }
     }
   }
